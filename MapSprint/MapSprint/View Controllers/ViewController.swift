6f1891c93f681ffadfda228648b35a09700e5d30
John Holowesko <john@sipsip.io>
2020-07-19 22:30:15 -0400

    Initial Commit

---
+++ MapSprint/MapSprint/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapSprint
+//
+//  Created by John Holowesko on 7/19/20.
+//  Copyright © 2020 John Holowesko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
